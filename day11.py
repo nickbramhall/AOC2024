0aa29c64221dc3438b4d15bd75dd1e93ffb266c6
Nick Bramhall <n.j.bramhall@gmail.com>
2024-12-11 14:47:44 +0000

    Adding in some more comments

--- day11.py
+++ day11.py
@@ -15,16 +15,21 @@ for line in all_lines:
 
 print(stones)
 
+# Use a lru_cache from functools to avoid recalculating the change to a stone - if the value exists in the cache it will be returned without re-calculating
+# A list is returned so that it can be iterated over whenter it returns 1 stone (rules 1 and 3) or 2 stones (rule 2)
 @lru_cache(maxsize = 128)
 def blink(stone):
     stone_string=str(stone)
+    # First rule - if Stone has 0 on it, the new Stone has 1 on it
     if stone_string == '0':
         return [1]
-    if len(stone_string) % 2 == 0: 
+    # Second rule - if Stone has number on it than be evenly divided, then two new stones are created with the first half and last half
+    elif len(stone_string) % 2 == 0: 
         length_of_stone=len(stone_string)
         splitat=length_of_stone // 2
         left, right = stone_string[:splitat], stone_string[splitat:]
         return [int(left), int(right)]
+    # Otherwise a new stone is created with the value of the new stone 2024 times the original stone's value
     else:
         value = int(stone) * 2024
         return [value]
@@ -50,7 +55,7 @@ def many_blinks(rounds):
                         duplicate_stones_dict[f'{item}']=v
     # Now add up all the values in the stones dictionary to get the total number of stones
     sum=0
-    for k,v in duplicate_stones_dict.items():
+    for v in duplicate_stones_dict.values():
         sum = sum + v
     return sum
 
